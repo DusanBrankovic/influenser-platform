74d48f3faec278c5952d3b526ea5f6154e0b9703
DusanBrankovic <72136195+DusanBrankovic@users.noreply.github.com>
2026-01-12 12:45:55 +0100

    Revert "setup for backend hosting"

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -2,15 +2,15 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ConfigService } from '@nestjs/config';
+import { jwtConstants } from './dto/credentials.dto';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configSModule: ConfigService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configSModule.get<string>("JWT_SECRET") || '',
+      secretOrKey: jwtConstants.secret,
     });
   }
 
