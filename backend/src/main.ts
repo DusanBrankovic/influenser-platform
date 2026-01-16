ee38b4a672b5c304f09e8dc1b5077807678c7f32
DusanBrankovic <dbrankovic3819rn@raf.rs>
2026-01-16 12:44:26 +0100

    Added CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://influenser-platform-app.vercel.app'
+    ],
     credentials: true,
   });
   
