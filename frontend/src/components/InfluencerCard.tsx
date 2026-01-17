48ee071d71b209ce0035a96adf09c42215f3736e
DusanBrankovic <dbrankovic3819rn@raf.rs>
2026-01-17 04:02:34 +0100

    Cleaned up influencer card

--- frontend/src/components/InfluencerCard.tsx
+++ frontend/src/components/InfluencerCard.tsx
@@ -1,7 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card"
-import { BadgeCheck, Bookmark, Star } from "lucide-react"
-import { Button } from "./ui/button";
-import { Separator } from "./ui/separator";
+import { BadgeCheck } from "lucide-react"
 import MockImage from "@/assets/img/MockImage";
 import type { Influencer } from "@/types/influencer.types";
 
@@ -11,7 +9,6 @@ type ProfileCardProps = {
 
 export default function InfluencerCard({ influencer }: ProfileCardProps) {
 
-// const safeRating = Math.max(0, Math.min(influencer.rating, influencer.ratingOutOf));
   return (
     <Card className="w-full max-w-[420px] overflow-hidden rounded-3xl bg-white shadow-sm">
       <CardContent className="p-4 sm:p-5">
@@ -32,35 +29,6 @@ export default function InfluencerCard({ influencer }: ProfileCardProps) {
                 {influencer.experience}
             </p>
           </div>
-        </div>
-
-        <Separator className="my-4" />
-
-        <div className="flex items-start justify-between gap-3">
-          {/* <div className="min-w-0 flex-1">
-            <div className="mt-2 flex flex-wrap gap-2">
-              {tags.map((t, idx) => (
-                <Badge
-                  key={`${t}-${idx}`}
-                  variant="secondary"
-                  className="rounded-md bg-black/45 text-white hover:bg-black/50"
-                >
-                  {t}
-                </Badge>
-              ))}
-            </div>
-          </div> */}
-
-          <Button
-            type="button"
-            variant="outline"
-            size="icon"
-            onClick={() => {}}
-            className="shrink-0 rounded-xl border-black/10"
-            aria-label="Bookmark"
-          >
-            {/* <Bookmark className={`h-5 w-5 ${influencer.bookmarked ? "fill-black text-black" : "text-black/70"}`} /> */}
-          </Button>
         </div>
       </CardContent>
     </Card>
