37a3ff3c7b6f42cb0ec70be074aebc7e116de7bd
DusanBrankovic <dbrankovic3819rn@raf.rs>
2026-02-07 13:25:34 +0100

    translated register options

--- frontend/src/utils/accountTypes.ts
+++ frontend/src/utils/accountTypes.ts
@@ -1,21 +1,22 @@
 import { UserRole } from "@/types/auth.types";
 import type { AccountTypesProps } from "./AccountTypes.props";
+
 export const accountTypes: AccountTypesProps[] = [
   {
     id: "business",
     role: UserRole.BUSINESS,
-    title: "Ja sam biznis",
+    title: "I am a business",
     description:
-      "Želim da pretražim influensere i kontaktiram izabranog influensera za dalju saradnju",
+      "I want to search for influencers and contact selected influencers for potential collaboration",
     icon: "domain",
     fill: "rounded",
   },
   {
     id: "influencer",
     role: UserRole.INFLUENCER,
-    title: "Ja sam influenser",
+    title: "I am an influencer",
     description:
-      "Želim da kreiram svoj profil na kome mogu prikazati rezultate svojih saradnji",
+      "I want to create my profile where I can showcase the results of my collaborations",
     icon: "id_card",
     fill: "outlined",
   },
